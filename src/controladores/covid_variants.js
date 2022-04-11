75cf10159d84668955e072e59f54d875628479a6
Luana Lacerda <luanaretifica@hotmail.com>
2022-04-11 20:29:49 -0300

    Adicionando para teste rota /dates

--- src/controladores/covid_variants.js
+++ src/controladores/covid_variants.js
@@ -1,25 +1,53 @@
+const conexao = require('../conexao');
+
 async function getTest(req, res) {
     res.status(200).json('Backend Challenge 2021 🏅 - Covid Daily Cases')
 };
 
 // async function listarDadosDia(req, res) {
+//     const date = req.params.date
+
+//     try {
+//         const query = 'select * from covid_variants where date = $1'
+//         const {rows, rowCount} = await conexao.query(query,[date])
+
+//         if(rowCount <=0){
+//             return res.status(404).json({messagem: "Não foi encontrado para a data fornecida"})
+//         }
+
+//     } catch (error) {
+//         return res.status(500).json({ mensagem: "Ocorreu um erro inesperado. - " + error.message })
+
+//     }
+
 
 // };
 
-// async function listarSomaCasos(req, res) {
+async function listarSomaCasos(req, res) {
 
-// }
+}
 
-// async function listarDatas(req, res) {
+async function listarDatasDisponiveis(req, res) {
 
-// }
+    try {
+        const query = 'select distinct date from covid_variants'
+        const { rows } = await conexao.query(query)
+
+        return res.json(200).json(rows)
+    } catch (error) {
+        return res.status(500).json({ mensagem: "Ocorreu um erro inesperado. - " + error.message })
+
+    }
+
+
+}
 
 
 module.exports = {
     getTest,
-    // listarDadosDia,
-    // listarSomaCasos,
-    // listarDatas
+    //listarDadosDia,
+    listarSomaCasos,
+    listarDatasDisponiveis
 
 
 }
--- src/rotas.js
+++ src/rotas.js
@@ -4,8 +4,8 @@ const controladorCovidVariants = require('./controladores/covid_variants');
 const rotas = express()
 
 rotas.get('/', controladorCovidVariants.getTest);
-// rotas.get('/cases/:date/count', controladorCovidVariants.listarDadosDia);
-// rotas.get('/cases/:date/cumulative', controladorCovidVariants.listarDadosDia);
-// rotas.get(':date', controladorCovidVariants.listarDadosDia);
+//rotas.get('/cases/:date/count', controladorCovidVariants.listarDadosDia);
+rotas.get('/cases/:date/cumulative', controladorCovidVariants.listarSomaCasos);
+rotas.get('/date', controladorCovidVariants.listarDatasDisponiveis);
 
 module.exports = rotas;
