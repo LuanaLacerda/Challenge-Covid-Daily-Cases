1e6450a150837cb623e77de669e49a35e89dac83
Luana Lacerda <luanaretifica@hotmail.com>
2022-04-13 10:48:17 -0300

    Arquivo servidor atualizado!

--- src/servidor.js
+++ src/servidor.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const rotas = require('./rotas');
-const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
 app.use(rotas);
 
 module.exports = app;
